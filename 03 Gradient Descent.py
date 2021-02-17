649fd796a1894bf8182af5812fa58ff0e6772077
Tim Chung <31941027+timhyc19@users.noreply.github.com>
2021-02-17 16:45:21 -0500

    Add files via upload

---
+++ 03 Gradient Descent.py
@@ -0,0 +1,740 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Notebook Imports and Packages
+
+# In[131]:
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from mpl_toolkits.mplot3d.axes3d import Axes3D
+from matplotlib import cm # color map
+
+from sympy import symbols, diff
+from math import log
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# ### Example 1 - A simple cost function
+# $f(x) = x^2 + x + 1$
+
+# In[132]:
+
+
+def f(x):
+    return x**2 + x + 1
+
+
+# In[133]:
+
+
+# Make Data
+x_1 = np.linspace(start=-3, stop=3, num=500)
+
+
+# In[134]:
+
+
+# Plot
+plt.xlim([-3, 3])
+plt.ylim(0, 8)
+plt.xlabel('X', fontsize=16)
+plt.ylabel('f(x)', fontsize=16)
+plt.plot(x_1, f(x_1))
+plt.show()
+
+
+# ### Slope & Derivates
+
+# In[135]:
+
+
+def df(x):
+    return 2*x + 1
+
+
+# In[136]:
+
+
+# Plot Function and derivative side by side
+
+plt.figure(figsize=[15, 5])
+
+#1 Chart: Cost Function
+plt.subplot(1, 2, 1)
+
+plt.xlim([-3, 3])
+plt.ylim(0, 8)
+plt.title('Cost function', fontsize=17)
+plt.xlabel('X', fontsize=16)
+plt.ylabel('f(x)', fontsize=16)
+plt.plot(x_1, f(x_1), color='blue', linewidth=3)
+
+#2 Chart: Derivative
+plt.subplot(1, 2, 2)
+
+plt.title('Slope of the cost function', fontsize=17)
+plt.xlabel('X', fontsize=16)
+plt.ylabel('df(x)', fontsize=16)
+plt.grid()
+plt.xlim([-2, 3])
+plt.ylim(-3, 6)
+
+plt.plot(x_1, df(x_1), color='skyblue', linewidth=5)
+
+plt.show()
+
+
+# ## Python Loops & Gradient Descent
+
+# In[137]:
+
+
+# Python For Loop
+for n in range(5):
+    print('Hello world', n)
+print('End of loop')
+
+
+# In[138]:
+
+
+# Python while loop
+counter = 0
+while counter < 5:
+    print('Counting ...', counter)
+    counter += 1
+print('Ready or not, here I come!')
+
+
+# In[139]:
+
+
+# Gradient Descent
+new_x = 3
+previous_x = 0
+step_multiplier = 0.1
+precision = 0.00001
+
+x_list = [new_x]
+slope_list = [df(new_x)]
+
+for n in range(500):
+    previous_x = new_x 
+    gradient = df(previous_x)
+    new_x = previous_x - step_multiplier * gradient
+    
+    step_size = abs(new_x - previous_x)
+    # print(step_size)
+    
+    x_list.append(new_x)
+    slope_list.append(df(new_x))
+    
+    if step_size < precision:
+        print('Loop ran this many times:', n)
+        break
+    
+print('Local minimum occurs at:', new_x)
+print('Slope of df(x) value at this point is:', df(new_x))
+print('f(x) value or cost at this point is:', f(new_x))
+
+
+# In[140]:
+
+
+# Superimpose the gradent descent calculations on plot
+
+plt.figure(figsize=[20, 5])
+
+#1 Chart: Cost Function
+plt.subplot(1, 3, 1)
+
+plt.xlim([-3, 3])
+plt.ylim(0, 8)
+plt.title('Cost function', fontsize=17)
+plt.xlabel('X', fontsize=16)
+plt.ylabel('f(x)', fontsize=16)
+plt.plot(x_1, f(x_1), color='blue', linewidth=3, alpha=0.8)
+
+values = np.array(x_list)
+plt.scatter(x_list, f(values), color='red', s=100, alpha=0.6)
+
+#2 Chart: Derivative
+plt.subplot(1, 3, 2)
+
+plt.title('Slope of the cost function', fontsize=17)
+plt.xlabel('X', fontsize=16)
+plt.ylabel('df(x)', fontsize=16)
+plt.grid()
+plt.xlim([-2, 3])
+plt.ylim(-3, 6)
+
+plt.plot(x_1, df(x_1), color='skyblue', linewidth=5, alpha=0.6)
+plt.scatter(x_list, slope_list, color='red', s=100, alpha=0.5)
+
+#3 Chart: Derivative (Close up)
+plt.subplot(1, 3, 3)
+
+plt.title('Gradient Descent (close up)', fontsize=17)
+plt.xlabel('X', fontsize=16)
+plt.grid()
+plt.xlim(-0.55, -0.2)
+plt.ylim(-0.3, 0.8)
+
+plt.plot(x_1, df(x_1), color='skyblue', linewidth=6, alpha=0.8)
+plt.scatter(x_list, slope_list, color='red', s=300, alpha=0.5)
+
+plt.show()
+
+
+# # Example 2 - Multiple Minima vs Initial Guess & Advanced Functions
+# ## $g(x) = x^4 - 4x^2 + 5$
+
+# In[141]:
+
+
+#Make some data
+x_2 = np.linspace(-2, 2, 1000)
+
+def g(x):
+    return x**4 - 4*x**2 + 5
+
+def dg(x):
+    return 4*x**3 - 8*x
+
+
+# In[142]:
+
+
+# Plot Function and derivative side by side
+
+plt.figure(figsize=[15, 5])
+
+#1 Chart: Cost Function
+plt.subplot(1, 2, 1)
+
+plt.xlim(-2, 2)
+plt.ylim(0.5, 5.5)
+plt.title('Cost function', fontsize=17)
+plt.xlabel('X', fontsize=16)
+plt.ylabel('g(x)', fontsize=16)
+plt.plot(x_2, g(x_2), color='blue', linewidth=3)
+
+#2 Chart: Derivative
+plt.subplot(1, 2, 2)
+
+plt.title('Slope of the cost function', fontsize=17)
+plt.xlabel('X', fontsize=16)
+plt.ylabel('dg(x)', fontsize=16)
+plt.grid()
+plt.xlim([-2, 2])
+plt.ylim(-6, 8)
+
+plt.plot(x_2, dg(x_2), color='skyblue', linewidth=5)
+
+plt.show()
+
+
+# ## Gradient Descent as a Python Function
+
+# In[143]:
+
+
+def gradient_descent(derivative_func, initial_guess, multiplier=0.02, precision=0.001, 
+                    max_iter=300):
+    new_x = initial_guess
+    x_list = [new_x]
+    slope_list = [derivative_func(new_x)]
+
+    for n in range(max_iter):
+        previous_x = new_x 
+        gradient = derivative_func(previous_x)
+        new_x = previous_x - multiplier * gradient
+
+        step_size = abs(new_x - previous_x)
+        x_list.append(new_x)
+        slope_list.append(derivative_func(new_x))
+
+        if step_size < precision:
+            break
+    return new_x, x_list, slope_list
+
+
+# In[144]:
+
+
+local_min, list_x, deriv_list = gradient_descent(dg, 0.5, 0.02, 0.001)
+print('Local min occurs at:', local_min)
+print('Number of steps:', len(list_x))
+
+
+# In[145]:
+
+
+local_min, list_x, deriv_list = gradient_descent(derivative_func=dg, initial_guess=-0.5, 
+                                                 multiplier=0.01, precision=0.0001)
+print('Local min occurs at:', local_min)
+print('Number of steps:', len(list_x))
+
+
+# In[146]:
+
+
+local_min, list_x, deriv_list = gradient_descent(derivative_func=dg, initial_guess=-0.1)
+print('Local min occurs at:', local_min)
+print('Number of steps:', len(list_x))
+
+
+# In[147]:
+
+
+# Calling gradient descent function
+local_min, list_x, deriv_list = gradient_descent(derivative_func=dg, initial_guess=0.1)
+
+# Plot Function and derivative and scatter plot side by side
+
+plt.figure(figsize=[15, 5])
+
+#1 Chart: Cost Function
+plt.subplot(1, 2, 1)
+
+plt.xlim(-2, 2)
+plt.ylim(0.5, 5.5)
+plt.title('Cost function', fontsize=17)
+plt.xlabel('X', fontsize=16)
+plt.ylabel('g(x)', fontsize=16)
+plt.plot(x_2, g(x_2), color='blue', linewidth=3, alpha=0.8)
+plt.scatter(list_x, g(np.array(list_x)), color='red', s=100, alpha=0.6)
+
+#2 Chart: Derivative
+plt.subplot(1, 2, 2)
+
+plt.title('Slope of the cost function', fontsize=17)
+plt.xlabel('X', fontsize=16)
+plt.ylabel('dg(x)', fontsize=16)
+plt.grid()
+plt.xlim([-2, 2])
+plt.ylim(-6, 8)
+
+plt.plot(x_2, dg(x_2), color='skyblue', linewidth=5)
+plt.scatter(list_x, deriv_list, color='red', s=100, alpha=0.5)
+
+plt.show()
+
+
+# ### Example 3 - Divergence, Overflow and Python Tuples
+# ### $h(x) = x^5 -2x^4 + 2$
+
+# In[148]:
+
+
+# Make data
+x_3 = np.linspace(start=-2.5, stop=2.5, num=1000)
+
+
+# In[149]:
+
+
+def h(x):
+    return x**5 - 2*x**4 + 2
+
+def dh(x):
+    return 5*x**4 -8*x**3
+
+
+# In[150]:
+
+
+# Calling gradient descent function
+local_min, list_x, deriv_list = gradient_descent(derivative_func=dh, initial_guess=-0.2,
+                                                max_iter=71)
+
+# Plot Function and derivative and scatter plot side by side
+
+plt.figure(figsize=[15, 5])
+
+#1 Chart: Cost Function
+plt.subplot(1, 2, 1)
+
+plt.xlim(-1.2, 2.5)
+plt.ylim(-1, 4)
+plt.title('Cost function', fontsize=17)
+plt.xlabel('X', fontsize=16)
+plt.ylabel('h(x)', fontsize=16)
+plt.plot(x_3, h(x_3), color='blue', linewidth=3, alpha=0.8)
+plt.scatter(list_x, h(np.array(list_x)), color='red', s=100, alpha=0.6)
+
+#2 Chart: Derivative
+plt.subplot(1, 2, 2)
+
+plt.title('Slope of the cost function', fontsize=17)
+plt.xlabel('X', fontsize=16)
+plt.ylabel('hg(x)', fontsize=16)
+plt.grid()
+plt.xlim([-1, 2])
+plt.ylim(-4, 5)
+
+plt.plot(x_3, dh(x_3), color='skyblue', linewidth=5)
+plt.scatter(list_x, deriv_list, color='red', s=100, alpha=0.5)
+
+plt.show()
+
+print('Local min occurs at: ', local_min)
+print('Cost at this minimum is: ', h(local_min))
+print('Number of steps: ', len(list_x))
+
+
+# In[151]:
+
+
+import sys
+#help(sys)
+#sys.version
+type(h(local_min))
+sys.float_info.max
+
+
+# ### Python Tuples
+
+# In[152]:
+
+
+# Creating a tuple
+breakfast = 'bacon', 'eggs', 'avocada'
+unlucky_numbers = 13, 4, 9, 26, 17
+
+print('I love ', breakfast[0])
+print('My hotel has no ' + str(unlucky_numbers[1]) + 'th floor')
+
+not_my_address = 1, 'Infinite Loop', 'Cupertino', 95014
+
+tuple_with_single_value = 42, 
+type(tuple_with_single_value)
+
+main, side, greens = breakfast
+print('Main course is ', main)
+
+data_tuple = gradient_descent(derivative_func=dh, initial_guess=0.2)
+print('Local min is', data_tuple[0])
+print('Cost at the last x value is', h(data_tuple[0]))
+print('Number of steps is', len(data_tuple[1]))
+
+
+# In[153]:
+
+
+# The Learning Rate
+
+
+# In[154]:
+
+
+# Calling gradient descent function
+local_min, list_x, deriv_list = gradient_descent(derivative_func=dg, initial_guess=1.9,
+                                                multiplier=0.02, max_iter=500)
+
+# Plot Function and derivative and scatter plot side by side
+
+plt.figure(figsize=[15, 5])
+
+#1 Chart: Cost Function
+plt.subplot(1, 2, 1)
+
+plt.xlim(-2, 2)
+plt.ylim(0.5, 5.5)
+plt.title('Cost function', fontsize=17)
+plt.xlabel('X', fontsize=16)
+plt.ylabel('g(x)', fontsize=16)
+plt.plot(x_2, g(x_2), color='blue', linewidth=3, alpha=0.8)
+plt.scatter(list_x, g(np.array(list_x)), color='red', s=100, alpha=0.6)
+
+#2 Chart: Derivative
+plt.subplot(1, 2, 2)
+
+plt.title('Slope of the cost function', fontsize=17)
+plt.xlabel('X', fontsize=16)
+plt.ylabel('dg(x)', fontsize=16)
+plt.grid()
+plt.xlim([-2, 2])
+plt.ylim(-6, 8)
+
+plt.plot(x_2, dg(x_2), color='skyblue', linewidth=5)
+plt.scatter(list_x, deriv_list, color='red', s=100, alpha=0.5)
+
+plt.show()
+
+print('Number of steps: ', len(list_x))
+
+
+# In[155]:
+
+
+# Run gradient descent 3 times
+n = 100
+low_gamma = gradient_descent(derivative_func=dg, initial_guess=3,
+                                                multiplier=0.0005, precision=0.0001,
+                                                 max_iter=n)
+
+mid_gamma = gradient_descent(derivative_func=dg, initial_guess=3,
+                                                multiplier=0.001, precision=0.0001,
+                                                 max_iter=n)
+
+high_gamma = gradient_descent(derivative_func=dg, initial_guess=3,
+                                                multiplier=0.002, precision=0.0001,
+                                                 max_iter=n)
+
+# Experiment
+insane_gamma = gradient_descent(derivative_func=dg, initial_guess=1.9,
+                                                multiplier=0.25, precision=0.0001,
+                                                 max_iter=n)
+
+# Plotting reduction in cost for each iteration
+plt.figure(figsize=[20, 10])
+
+plt.xlim(0, n)
+plt.ylim(0, 50)
+plt.title('Effect of the learning rate', fontsize=17)
+plt.xlabel('Nr of iterations', fontsize=16)
+plt.ylabel('Cost', fontsize=16)
+
+# Values for our charts
+# 1) Y Axis Data: convert the lists to numpy arrays
+low_values = np.array(low_gamma[1])
+
+
+# 2) X Axis Data: create a list from 0 to n+1
+iteration_list = list(range(0, n+1))
+
+# Plotting low learning rate
+plt.plot(iteration_list, g(low_values), color='lightgreen', linewidth=5)
+plt.scatter(iteration_list, g(low_values), color='lightgreen', s=80)
+
+# Plotting mid learning rate
+plt.plot(iteration_list, g(np.array(mid_gamma[1])), color='steelblue', linewidth=5)
+plt.scatter(iteration_list, g(np.array(mid_gamma[1])), color='steelblue', s=80)
+
+# Plotting high learning rate
+plt.plot(iteration_list, g(np.array(high_gamma[1])), color='hotpink', linewidth=5)
+plt.scatter(iteration_list, g(np.array(high_gamma[1])), color='hotpink', s=80)
+
+# Plotting insane learning rate
+plt.plot(iteration_list, g(np.array(insane_gamma[1])), color='red', linewidth=5)
+plt.scatter(iteration_list, g(np.array(insane_gamma[1])), color='red', s=80)
+
+plt.show()
+
+
+# ### Example 4: Data Visualization with 3D Charts
+# ### Minimize $f(x,y) = \frac{1}{3^{-x^2 - y^2} + 1}$
+# Minimise $f(x,y) = \frac{1}{r+1}$ where $r$ is $3^{-x^2-y^2}$
+
+# In[156]:
+
+
+def f(x,y):
+    r = 3**(-x**2 - y**2)
+    return 1 / (r+1)
+
+
+# In[157]:
+
+
+# Make our x and y data
+x_4 = np.linspace(start=-2, stop=2, num=200)
+y_4 = np.linspace(start=-2, stop=2, num=200)
+
+print('Shape of X array', x_4.shape)
+
+x_4, y_4 = np.meshgrid(x_4, y_4)
+print('Array after meshgrid: ', x_4.shape)
+
+
+# In[158]:
+
+
+# Generating 3D Plot
+fig = plt.figure(figsize=[16, 12])
+ax = fig.gca(projection='3d')
+
+ax.set_xlabel('X', fontsize=20)
+ax.set_ylabel('Y', fontsize=20)
+ax.set_zlabel('f(x,y) - Cost', fontsize=20)
+
+ax.plot_surface(x_4, y_4, f(x_4, y_4), cmap=cm.coolwarm, alpha=0.4)
+
+plt.show()
+
+
+# ### Partial Derivatives & Symbolic Computation
+# 
+# ### $\frac{\partial f}{\partial x} = \frac{2x \ln(3) \cdot 3^{-x^2 - y^2}}{\left(3^{-x^2-y^2} + 1 \right)^2}$
+# 
+# 
+# ### $\frac{\partial f}{\partial y} = \frac{2y \ln(3) \cdot 3^{-x^2 - y^2}}{\left(3^{-x^2-y^2} + 1 \right)^2}$
+
+# In[159]:
+
+
+a, b = symbols('x, y')
+print('Our cost function f(x,y) is: ', f(a, b))
+print('Partial derivative wrt x is: ', diff(f(a,b), a))
+print('Value of f(x,y) at x=1.8 y =1.0 is:', f(a,b).evalf(subs={a:1.8, b:1.0})) # Python dictionary
+print('Value of partial derivatives wrt x: ', diff(f(a,b), a).evalf(subs={a: 1.8, b:1.0}))
+
+
+# ### Batch Gradient Descent with SymPy
+
+# In[167]:
+
+
+# Setup
+multiplier = 0.1
+max_iter = 500
+params = np.array([1.8, 1.0]) # Initial guess
+
+for n in range(max_iter):
+    gradient_x = diff(f(a,b), a).evalf(subs={a:params[0], b:params[1]})
+    gradient_y = diff(f(a,b), b).evalf(subs={a:params[0], b:params[1]})
+    gradients = np.array([gradient_x, gradient_y])
+    params = params - multiplier * gradients
+    #values_array = np.append(values_array, params.reshape(1, 2), axis=0)
+    values_array=np.concatenate((values_array, params.reshape(1,2)), axis=0)
+    
+# Results
+print('Values in gradient array', gradients)
+print('Minimum occurs at x value of: ', params[0])
+print('Minimum occurs at y value of: ', params[1])
+print('The cost is: ', f(params[0], params[1]))
+
+
+# In[161]:
+
+
+# Generating 3D Plot
+fig = plt.figure(figsize=[16, 12])
+ax = fig.gca(projection='3d')
+
+ax.set_xlabel('X', fontsize=20)
+ax.set_ylabel('Y', fontsize=20)
+ax.set_zlabel('f(x,y) - Cost', fontsize=20)
+
+ax.plot_surface(x_4, y_4, f(x_4, y_4), cmap=cm.coolwarm, alpha=0.4)
+ax.scatter(values_array[:, 0], values_array[:, 1],
+           f(values_array[:, 0],values_array[:, 1]), s=50, color='red')
+
+plt.show()
+
+
+# In[162]:
+
+
+# Partial Derivative functions example 4
+def fpx(x, y):
+    r = 3**(-x**2 - y**2)
+    return 2*x*log(3)*r / (r+1)**2
+
+def fpy(x, y):
+    r = 3**(-x**2 - y**2)
+    return 2*y*log(3)*r / (r+1)**2
+
+
+# In[163]:
+
+
+# Setup
+multiplier = 0.1
+max_iter = 500
+params = np.array([1.8, 1.0]) # Initial guess
+
+for n in range(max_iter):
+    gradient_x = fpx(params[0], params[1])
+    gradient_y = fpy(params[0], params[1])
+    gradients = np.array([gradient_x, gradient_y])
+    params = params - multiplier * gradients
+    
+# Results
+print('Values in gradient array', gradients)
+print('Minimum occurs at x value of: ', params[0])
+print('Minimum occurs at y value of: ', params[1])
+print('The cost is: ', f(params[0], params[1]))
+
+
+# In[164]:
+
+
+### Graphing 3D Gradient Descent & Advanced Numpy Arrays
+
+
+# In[165]:
+
+
+# Setup
+multiplier = 0.1
+max_iter = 500
+params = np.array([1.8, 1.0]) # Initial guess
+values_array = params.reshape(1, 2)
+print(values_array.shape)
+
+for n in range(max_iter):
+    gradient_x = fpx(params[0], params[1])
+    gradient_y = fpy(params[0], params[1])
+    gradients = np.array([gradient_x, gradient_y])
+    params = params - multiplier * gradients
+    
+    
+# Results
+print('Values in gradient array', gradients)
+print('Minimum occurs at x value of: ', params[0])
+print('Minimum occurs at y value of: ', params[1])
+print('The cost is: ', f(params[0], params[1]))
+
+
+# In[166]:
+
+
+# Advanced Numpy Array Practice:
+
+kirk = np.array([['Captain', 'Guitar']])
+print(kirk.shape)
+
+hs_band = np.array([['Black Thought', 'MC'], ['Questlove', 'Drums']])
+print(hs_band.shape)
+
+print('hs_band[0]   :', hs_band[0])
+print('hs_band[0][1]   :', hs_band[1][0])
+
+the_roots = np.append(arr = hs_band, values=kirk, axis=0)
+print(the_roots)
+
+print('Printing nicknames...', the_roots[:, 0])
+
+the_roots = np.append(arr = the_roots, values=[['Malik B', 'MC']], axis=0)
+print('Printing band roles ...', the_roots[:, 1])
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
